b8247e6d3127f5316593e96effe8538e23a2b044
GregoryPetrovLn <grishap96123@gmail.com>
2023-05-01 17:28:15 +0300

    added script

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const video = document.getElementById("video");
 
 
 Promise.all([
+  faceapi.nets.ssdMobilenetv1.loadFromUri("/models"),
   faceapi.nets.tinyFaceDetector.loadFromUri("/models"),
   faceapi.nets.faceLandmark68Net.loadFromUri("/models"),
   faceapi.nets.faceRecognitionNet.loadFromUri("/models"),
