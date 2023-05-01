86ae327e14637ed65df1055cbde48fa273857cb2
GregoryPetrovLn <grishap96123@gmail.com>
2023-05-01 17:07:19 +0300

    added event listener

--- script.js
+++ script.js
@@ -1,12 +1,28 @@
 const video = document.getElementById("video");
 
+
+Promise.all([
+  faceapi.nets.tinyFaceDetector.loadFromUri("/models"),
+  faceapi.nets.faceLandmark68Net.loadFromUri("/models"),
+  faceapi.nets.faceRecognitionNet.loadFromUri("/models"),
+  faceapi.nets.faceExpressionNet.loadFromUri("/models"),
+]).then(startWebcam);
+
 function startWebcam() {
-  navigator.mediaDevices
-    .getUserMedia({ video: true, audio: false })
-    .then((stream) => {
-      video.srcObject = stream;
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+  navigator.getUserMedia(
+    { video: {} },
+    (stream) => (video.srcObject = stream),
+    (err) => console.log(err)
+  );
 }
+
+video.addEventListener("play", () => {
+  setInterval(async () => {
+    const detections = await faceapi
+      .detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
+      .withFaceLandmarks()
+      .withFaceExpressions();
+  }, 100);
+});
+
+startWebcam();
